c8a89b3b2beab0ea9419f75b35d2a19e0f5958ee
Bess Chan <bess_chan@apple.com>
2015-08-04 23:06:52 -0700

    modified main method to initialize game controller

--- Blackjack.oop/main.m
+++ Blackjack.oop/main.m
@@ -7,42 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "RoundController.h"
+#import "GameController.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        printf("Welcome to command line blackjack!\n");
-        printf("Please enter your name: ");
-        
-        //Get player's name
-        char userInput[100];
-        scanf("%s", &userInput);
-        
-        //initialize player and dealer
-        Player *player = [[Player alloc] init];
-        Dealer *dealer = [[Dealer alloc] init];
-        Deck *deck = [[Deck alloc] init];
-        
-        player.name = [NSString stringWithCString: userInput encoding: NSASCIIStringEncoding];
-        dealer.name = @"Dealer";
-        [deck shuffle];
-
-        //Starting game
-        int roundNumber = 0;
-        while (YES) {
-            RoundController *round = [[RoundController alloc] initWithPlayer:player dealer:dealer deck:deck];
-            round.number = roundNumber;
-            [round start];
-            
-            printf("\nStart a new round?");
-            scanf("%s", &userInput);
-            roundNumber++;
-            
-            //shuffle deck every 6 rounds
-            if (roundNumber%6 == 0) {
-                [deck shuffle];
-            }
-        }
+        GameController *game = [[GameController alloc]init];
+        [game start];
     }
     return 0;
 }
